eaa981e5e4da3b6926e390993a6e48eea4e34f97
MiracleVulkan <MiracleVulkan@gmail.com>
2018-03-28 15:08:15 +0800

    mark

--- project.js
+++ project.js
@@ -5,11 +5,11 @@
 window.onload = function () {
 
     var childNode = {
-        template:'<div ><img v-for="(item,index) in items" :src="item.imgUrl" v-on:click="say(item.imgUrl)"></div>',
+        template:'<div ><img v-for="(item,index) in items" :src="item.imgUrl" v-on:click="show()"></div>',
         props:['items'],
         methods:{
-            say:function (message) {
-                alert(message);
+            show:function () {
+               this.showModal = true;
             }
         }
     }
@@ -22,22 +22,22 @@ window.onload = function () {
             return {
                 items: [
                     {imgUrl:"./image/01.jpg"},
-                    {imgUrl:"./image/02.jpg"},
-                    {imgUrl:"./image/03.jpg"},
-                    {imgUrl:"./image/04.jpg"},
                 ]
             }
         },
     }
 
-    var temp = new Vue({
-        el:'#app',
-        components:{
-            'parent':parentNode
+
+    Vue.component('div-image',{
+        props:['items'],
+        // template:'<p @click="say()">{{counter}}</p>',
+        template:'<div><img v-for="item in items" :src="item.imgUrl" v-on:click="say()"></div>',
+        data: function () {
+                return { counter: this.total }
         },
         methods: {
-            say: function (message) {
-                alert(message)
+            say:function () {
+                this.$emit('show-modal')
             }
         }
     })
@@ -48,13 +48,40 @@ window.onload = function () {
 				  <div class="modal-background"></div>
 				  <div id="app" class="modal-content_new">
 				  	<div id="app1" class="box_new">
-				  		<span>默认模态框内容sssssssssssssssssssssssssssssssssssssssssaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaafdffffffffffffffffff</span>
+				  		<span>默认模态框内容</span>
 				  	</div>
 				  </div>
 				  <button class="modal-close" @click="$emit('fireclose')"></button>
 				</div>
 			`
     });
+
+
+    var temp = new Vue({
+        el:'#app',
+        data: {
+            showModal:false,
+            items: [
+                {imgUrl:"./image/01.jpg"},
+                {imgUrl:"./image/02.jpg"},
+                {imgUrl:"./image/03.jpg"},
+                {imgUrl:"./image/04.jpg"},
+            ]
+        },
+        components:{
+            // 'parent':parentNode
+        },
+        methods:{
+            show:function () {
+                this.showModal = true;
+            },
+            say:function () {
+                alert(111);
+            },
+        }
+    })
+
+
 
 
 }
